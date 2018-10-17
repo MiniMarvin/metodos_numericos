465d10e329b1e0f5f237e055f6ff190e688ec5b6
Caio Gomes <caiogomes34@gmail.com>
2018-10-17 18:59:11 -0300

    adding the logs to files but still need to modify the function to compute the next steps from bashforth, multon and backward differential

--- projeto.py
+++ projeto.py
@@ -10,7 +10,7 @@
 #############################
 ## Import libs
 #############################
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import string
 import re
 
@@ -31,27 +31,36 @@ def proc(t, y, string):
 	"""
 	return eval(string)
 
-def plot_graph(points):
+def plot_graph(points, f):
 	"""Plot the graph with matplotlib
 	
 	Args:
 	    points (list(float, float)): The list with points (t_n, y_n)
 	"""
 	t_axis, y_axis = zip(*points)
-	plt.pyplot.xlabel("t")
-	plt.pyplot.ylabel("f'(t) = " + f)
-	plt.pyplot.plot(t_axis, y_axis)
+	# plt.pyplot.xlabel("t")
+	# plt.pyplot.ylabel("f'(t) = " + f)
+	# plt.pyplot.plot(t_axis, y_axis)
+	plt.xlabel("t")
+	plt.ylabel("f'(t) = " + f)
+	plt.plot(t_axis, y_axis)
+	plt.show()
 
-def print_points(points):
+def print_points(points, f):
 	"""Plot the graph with matplotlib
 	
 	Args:
 	    points (list(float, float)): The list with points (t_n, y_n)
 	"""
 	for idx, (t, y) in enumerate(points):
-		print(idx, y)
-	print()
+		# print(idx, y)
+		log_write(str(idx) + " " + str(y))
+	log_write("")
 
+def log_write(string):
+	print(string)
+	with open("saida.txt", "a") as f:
+		f.write(string + "\n")
 
 #############################
 ## Numerical Methods
@@ -222,12 +231,14 @@ def adam_multon(y_set, t0, h, n, f, order):
 	coeficients = [
 		[1/2, 1/2],
 		[5/12, 2/3, -1/12],
-		[3/8, 19/24, -5/24],
+		[3/8, 19/24, -5/24, 1/24],
 		[251/720, 323/360, -11/30, 53/360, -19/720],
 		[95/288, 1427/1440, -133/240, 241/720, -173/1440, 3/160],
 		[19087/60480, 2713/2520, -15487/20160, 586/945, -6737/20160, 263/2520, -863/60480],
 		[5257/17280, 139849/120960, -4511/4480, 123133/120960, -88547/120960, 1537/4480, -11351/120960, 275/24192]
 	]
+
+	constants = [-1/2, 1/24, -19/720, 3/160, -863/60480, 275/24192, 33953/3628800]
 
 	if order < 1:
 		raise ValueError("orders lower than 1 not supported in Adams Multon Method")
@@ -240,34 +251,80 @@ def adam_multon(y_set, t0, h, n, f, order):
 		for j in range(0, n - order + 1): # iterate untill the last point from prediction
 			pt_set = list(reversed(points))
 			total = 0
-			
+
 			i_limit = len(coeficients[order - 2]) - 1
 			for i in range(0, len(coeficients[order - 2]) - 1):
 				total += coeficients[order - 2][i + 1]*proc(t0 + h*(j+ (i_limit - i - 1)), pt_set[i], f)
 
 			## get the next step
-			n_set = adam_bashforth(y_set, t0, h, order + 1, f, order - 1)
+			mx = min(len(pt_set), 8)
+			lst = list(reversed(pt_set[:mx]))
+			t_val = t0 + h*(max(len(pt_set) - mx, 0))
+			# n_set = adam_bashforth(lst, t_val, h, mx + 1, f, mx - 1)
+			n_set = euler_inverso(t0 + h*len(pt_set), pt_set[0], h, 2, f)
 			
 			## retrive the predicted point
-			_, y_val = n_set[-1] 
+			_, y_val = n_set[-1]
 
 			## Add the last point
 			total += coeficients[order - 2][0]*proc(t0 + h*(j+i_limit), y_val, f) # them add the predicted point
 
 			val = pt_set[0] + h*total
+			# val += constants[order - 1]
 			points.append(val)
 		
 		points = [(t0 + idx*h, y) for idx, y in enumerate(points)]
 
 	return points
 	
-def formula_inversa():
-	"""Summary
-	
-	Returns:
-		TYPE: Description
+def formula_inversa(y_set, t0, h, n, f, order):
 	"""
-	points = []
+	Computes the adams multon default method.
+	param y_set The set containing all the y values of the method in order. 
+				This parameter goes from n-k untill n in the array, means y_set[0] = y_{n-k}.
+	param t0    The t value from the first step, it is the step n-k.
+	param h     The step size.
+	param n     Number of steps to use in the method.
+	param f     The differential equation used.
+	param order The order of the Adams Bashforth method. Goes from second order to the seventh order.
+	
+	return      The set of computed points.
+	"""
+	points = list(y_set)
+
+	coeficients = [
+		[1, 1],
+		[4/3, -1/3, 2/3],
+		[18/11, -9/11, 2/11, 6/11],
+		[48/25, -36/25, 16/25, -3/25, 12/25],
+		[300/137, -300/137, 200/137, -75/137, 12/137, 60/137],
+		[360/147, -450/147, 400/147, -225/147, 72/147, -10/147, 60/147]
+	]
+
+	for j in range(0, n - order + 1):
+		pt_set = list(reversed(points))
+		total = 0
+
+		# computes the before points values
+		for i in range(0, order - 1):
+			total += pt_set[i]*coeficients[order - 1][i]
+
+		# here we suppose the next stop from adams
+		mn = min(8, len(pt_set))
+		y_set = list(reversed(pt_set[:mn]))
+		t_val = t0 + h*abs(len(y_set) - mn)
+		# _, y_val = adam_bashforth(y_set, t_val, h, mn + 1, f, mn - 1)[-1]
+
+		_, y_val = runge_kutta(t0 + h*len(pt_set), pt_set[0], h, 2, f)[-1]
+
+		# The differential value computed
+		other = h*coeficients[order - 1][-1]*proc(t0 + h*len(pt_set), y_val, f)
+
+		val = total + other
+		points.append(val)
+
+	points = [(t0 + h*idx, y) for idx, y in enumerate(points)]
+
 	return points
 
 def detect_method(line):
@@ -298,172 +355,244 @@ def route_method(line):
 	inputString = ' '.join(inputString.split(' ')[1:])
 
 	if method == "euler":
-		print("Metodo de Euler")
+		# print("Metodo de Euler")
+		log_write("Metodo de Euler")
 		y0, t0, h, n, f = inputString.split(" ")
 		y0, t0, h, n = float(y0), float(t0), float(h), int(n)
 
-		print("y(", t0, ")")
-		print("h =", h)
+		# print("y(", t0, ")")
+		string = "y(" + str(t0) + ")"
+		log_write(string)
+		# print("h =", h)
+		string = "h =" + str(h)
+		log_write(string)
 		points = euler(t0, y0, h, n + 1, f)
 
 	elif method == "euler_inverso":
-		print("Metodo de Euler Inverso")
+		# print("Metodo de Euler Inverso")
+		log_write("Metodo de Euler Inverso")
 		y0, t0, h, n, f = inputString.split(" ")
 		y0, t0, h, n = float(y0), float(t0), float(h), int(n)
 
-		print("y(", t0, ")")
-		print("h =", h)
+		# print("y(", t0, ")")
+		string = "y(" + str(t0) + ")"
+		log_write(string)
+		# print("h =", h)
+		string = "h =" + str(h)
+		log_write(string)
 		points = euler_inverso(t0, y0, h, n + 1, f)
 
 	elif method == "euler_aprimorado":
-		print("Metodo de Euler Aprimorado")
+		# print("Metodo de Euler Aprimorado")
+		log_write("Metodo de Euler Aprimorado")
 		y0, t0, h, n, f = inputString.split(" ")
 		y0, t0, h, n = float(y0), float(t0), float(h), int(n)
 
-		print("y(", t0, ")")
-		print("h =", h)
+		# print("y(", t0, ")")
+		string = "y(" + str(t0) + ")"
+		log_write(string)
+		# print("h =", h)
+		string = "h =" + str(h)
+		log_write(string)
 		points = euler_aprimorado(t0, y0, h, n + 1, f)
 
 	elif method == "runge_kutta":
-		print("Metodo de Runge-Kutta")
+		# print("Metodo de Runge-Kutta")
+		log_write("Metodo de Runge-Kutta")
 		y0, t0, h, n, f = inputString.split(" ")
 		y0, t0, h, n = float(y0), float(t0), float(h), int(n)
 
-		print("y(", t0, ")")
-		print("h =", h)
+		# print("y(", t0, ")")
+		string = "y(" + str(t0) + ")"
+		log_write(string)
+		# print("h =", h)
+		string = "h =" + str(h)
+		log_write(string)
 		points = runge_kutta(t0, y0, h, n + 1, f)
 
 	elif method == "adam_bashforth":
-		print("Metodo Adam-Bashforth")
+		# print("Metodo Adam-Bashforth")
+		log_write("Metodo Adam-Bashforth")
 		t0, h, n, f, order = inputString.split(" ")[-5:] ## The last five elements
 		t0, h, n, order = float(t0), float(h), int(n), int(order)
 
 		y_set = inputString.split(" ")[:-5]
 		y_set = [float(a) for a in y_set]
 
-		print("y(", t0, ")")
-		print("h =", h)
+		# print("y(", t0, ")")
+		string = "y(" + str(t0) + ")"
+		log_write(string)
+		# print("h =", h)
+		string = "h =" + str(h)
+		log_write(string)
+
 		points = adam_bashforth(y_set, t0, h, n + 1, f, order)
 
 	elif method == "adam_bashforth_by_euler":
-		print("Metodo Adam-Bashforth por Euler")
+		# print("Metodo Adam-Bashforth por Euler")
+		log_write("Metodo Adam-Bashforth por Euler")
 		y0, t0, h, n, f, order = inputString.split(" ")
 		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
 
 		points = euler(t0, y0, h, order, f)
 		_, y_set = zip(*points)
-		print(y_set)
 
 		points = adam_bashforth(y_set, t0, h, n + 1, f, order)
 
 	elif method == "adam_bashforth_by_euler_inverso":
-		print("Metodo Adam-Bashforth por Euler Inverso")
+		# print("Metodo Adam-Bashforth por Euler Inverso")
+		log_write("Metodo Adam-Bashforth por Euler Inverso")
 		y0, t0, h, n, f, order = inputString.split(" ")
 		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
 
 		points = euler_inverso(t0, y0, h, order, f)
 		_, y_set = zip(*points)
-		print(y_set)
 
 		points = adam_bashforth(y_set, t0, h, n + 1, f, order)
 
 	elif method == "adam_bashforth_by_euler_aprimorado":
-		print("Metodo Adam-Bashforth por Euler Aprimorado")
+		# print("Metodo Adam-Bashforth por Euler Aprimorado")
+		log_write("Metodo Adam-Bashforth por Euler Aprimorado")
 		y0, t0, h, n, f, order = inputString.split(" ")
 		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
 
 		points = euler_aprimorado(t0, y0, h, order, f)
 		_, y_set = zip(*points)
-		print(y_set)
 
 		points = adam_bashforth(y_set, t0, h, n + 1, f, order)
 
 	elif method == "adam_bashforth_by_runge_kutta":
-		print("Metodo Adam-Bashforth por Runge-Kutta")
+		# print("Metodo Adam-Bashforth por Runge-Kutta")
+		log_write("Metodo Adam-Bashforth por Runge-Kutta")
 		y0, t0, h, n, f, order = inputString.split(" ")
 		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
 
 		points = runge_kutta(t0, y0, h, order, f)
 		_, y_set = zip(*points)
-		print(y_set)
 
 		points = adam_bashforth(y_set, t0, h, n + 1, f, order)
 
 	elif method == "adam_multon":
-		print("Metodo Adam-Multon")
+		# print("Metodo Adam-Multon")
+		log_write("Metodo Adam-Multon")
 		t0, h, n, f, order = inputString.split(" ")[-5:] ## The last five elements
 		t0, h, n, order = float(t0), float(h), int(n), int(order)
 
 		y_set = inputString.split(" ")[:-5]
 		y_set = [float(a) for a in y_set]
 
-		print("y(", t0, ")")
-		print("h =", h)
+		# print("y(", t0, ")")
+		string = "y(" + str(t0) + ")"
+		log_write(string)
+		# print("h =", h)
+		string = "h =" + str(h)
+		log_write(string)
 		points = adam_multon(y_set, t0, h, n + 1, f, order)
 
 	elif method == "adam_multon_by_euler":
-		print("Metodo Adam-Multon por Euler")
+		# print("Metodo Adam-Multon por Euler")
+		log_write("Metodo Adam-Multon por Euler")
 		y0, t0, h, n, f, order = inputString.split(" ")
 		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
 
 		points = euler(t0, y0, h, order - 1, f)
 		_, y_set = zip(*points)
-		print(y_set)
 
 		points = adam_multon(y_set, t0, h, n + 1, f, order)
 
 	elif method == "adam_multon_by_euler_inverso":
-		print("Metodo Adam-Multon por Euler Inverso")
+		# print("Metodo Adam-Multon por Euler Inverso")
+		log_write("Metodo Adam-Multon por Euler Inverso")
 		y0, t0, h, n, f, order = inputString.split(" ")
 		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
 
 		points = euler_inverso(t0, y0, h, order - 1, f)
 		_, y_set = zip(*points)
-		print(y_set)
 
 		points = adam_multon(y_set, t0, h, n + 1, f, order)
 
 	elif method == "adam_multon_by_euler_aprimorado":
-		print("Metodo Adam-Multon por Euler Aprimorado")
+		# print("Metodo Adam-Multon por Euler Aprimorado")
+		log_write("Metodo Adam-Multon por Euler Aprimorado")
 		y0, t0, h, n, f, order = inputString.split(" ")
 		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
 
 		points = euler_aprimorado(t0, y0, h, order - 1, f)
 		_, y_set = zip(*points)
-		print(y_set)
 
 		points = adam_multon(y_set, t0, h, n + 1, f, order)
 
 	elif method == "adam_multon_by_runge_kutta":
-		print("Metodo Adam-Multon por Runge-Kutta")
+		# print("Metodo Adam-Multon por Runge-Kutta")
+		log_write("Metodo Adam-Multon por Runge-Kutta")
 		y0, t0, h, n, f, order = inputString.split(" ")
 		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
 
 		points = runge_kutta(t0, y0, h, order - 1, f)
 		_, y_set = zip(*points)
-		print(y_set)
 
 		points = adam_multon(y_set, t0, h, n + 1, f, order)
 
 	elif method == "formula_inversa":
-		print("formula_inversa")
-		# points = formula_inversa()
+		# print("Metodo Formula Inversa")
+		log_write("Metodo Formula Inversa")
+		t0, h, n, f, order = inputString.split(" ")[-5:] ## The last five elements
+		t0, h, n, order = float(t0), float(h), int(n), int(order)
+
+		y_set = inputString.split(" ")[:-5]
+		y_set = [float(a) for a in y_set]
+
+		# print("y(", t0, ")")
+		string = "y(" + str(t0) + ")"
+		log_write(string)
+		# print("h =", h)
+		string = "h =" + str(h)
+		log_write(string)
+		points = formula_inversa(y_set, t0, h, n + 1, f, order)
 
 	elif method == "formula_inversa_by_euler":
-		print("formula_inversa_by_euler")
-		# points = formula_inversa_by_euler()
+		# print("Metodo Formula Inversa por Euler")
+		log_write("Metodo Formula Inversa por Euler")
+		y0, t0, h, n, f, order = inputString.split(" ")
+		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
+
+		points = euler(t0, y0, h, order - 1, f)
+		_, y_set = zip(*points)
+
+		points = formula_inversa(y_set, t0, h, n + 1, f, order)
 
 	elif method == "formula_inversa_by_euler_inverso":
-		print("formula_inversa_by_euler_inverso")
-		# points = formula_inversa_by_euler_inverso()
+		# print("Metodo Formula Inversa por Euler Inverso")
+		log_write("Metodo Formula Inversa por Euler Inverso")
+		y0, t0, h, n, f, order = inputString.split(" ")
+		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
+
+		points = euler_inverso(t0, y0, h, order - 1, f)
+		_, y_set = zip(*points)
+
+		points = formula_inversa(y_set, t0, h, n + 1, f, order)
 
 	elif method == "formula_inversa_by_euler_aprimorado":
-		print("formula_inversa_by_euler_aprimorado")
-		# points = formula_inversa_by_euler_aprimorado()
+		# print("Metodo Formula Inversa por Euler Aprimorado")
+		log_write("Metodo Formula Inversa por Euler Aprimorado")
+		y0, t0, h, n, f, order = inputString.split(" ")
+		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
+
+		points = euler_aprimorado(t0, y0, h, order - 1, f)
+		_, y_set = zip(*points)
+
+		points = formula_inversa(y_set, t0, h, n + 1, f, order)
 
 	elif method == "formula_inversa_by_runge_kutta":
-		print("formula_inversa_by_runge_kutta")
-		# points = formula_inversa_by_runge_kutta()
+		# print("Metodo Formula Inversa por Runge-Kutta")
+		log_write("Metodo Formula Inversa por Runge-Kutta")
+		y0, t0, h, n, f, order = inputString.split(" ")
+		y0, t0, h, n, order = float(y0), float(t0), float(h), int(n), int(order)
+
+		points = runge_kutta(t0, y0, h, order - 1, f)
+		_, y_set = zip(*points)
+
+		points = formula_inversa(y_set, t0, h, n + 1, f, order)
 
 	return points
 
@@ -480,7 +609,14 @@ def main():
 			# Correct the problem from 4x -> 4*x
 			# valstr = re.sub(r'([0-9])([a-z])', r'\1*\2', f) 
 			points = route_method(line)
-			print_points(points)
+			with open("saida.txt", "a") as f:
+				print_points(points, f)
+
+
+			inputString = line.replace('\n', '').replace('\r', '')
+			inputString = ' '.join(inputString.split(' ')[1:])
+
+			# plot_graph(points, inputString.split(" ")[-1])
 
 
 if __name__ == "__main__":
